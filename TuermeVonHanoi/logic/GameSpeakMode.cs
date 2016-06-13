736f62a56be0e5ec7954e00927c5e32d109dd32d
William Reiher <reiherw@googlemail.com>
2016-06-13 18:01:30 +0200

    control game with speech

---
+++ TuermeVonHanoi/logic/GameSpeakMode.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Recognition;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuermeVonHanoi.logic
+{
+    class GameSpeakMode
+    {
+
+        private SpeechRecognitionEngine recognizer;
+
+        static private Grammar grammar = new Grammar(@"Resources\grammar.xml", "MoveIt");
+
+        public event EventHandler ValueChanged;
+
+        public GameSpeakMode()
+        {
+
+        }
+
+        public void start()
+        {
+            this.recognizer = new SpeechRecognitionEngine();
+            this.recognizer.SetInputToDefaultAudioDevice();
+
+            recognizer.UnloadAllGrammars();
+            recognizer.LoadGrammar(grammar);
+
+            recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized);
+
+            recognizer.RecognizeAsync(RecognizeMode.Multiple);
+        }
+
+        public void stop()
+        {
+            recognizer.UnloadAllGrammars();
+            recognizer.RecognizeAsyncCancel();
+        }
+
+        private void recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            //foreach (RecognizedWordUnit word in e.Result.Words)
+            //{
+                // You can change the minimun confidence level here
+                //if (word.Confidence > 0.1f)
+                //System.Console.WriteLine(word.Text);
+            //}
+
+            //System.Console.WriteLine(e.Result.Semantics.Value);
+
+            EventHandler handler = ValueChanged;
+            if (handler != null) handler(this, e);
+        }
+    }
+}
